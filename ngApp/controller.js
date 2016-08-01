2167e90e71e5d149e5809bdff18ce0abab608f0d
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-08-01 14:45:55 -0700

    added basic event handling

--- ngApp/controller.js
+++ ngApp/controller.js
@@ -4,15 +4,15 @@ var MovieApp;
     (function (Controllers) {
         var MovieController = (function () {
             function MovieController() {
-                this.dogs = [
-                    { name: 'German Shepherd', img: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Buck_The_GSD.jpg' },
-                    { name: 'Black Lab', img: 'https://i.ytimg.com/vi/GmXks--98Bc/maxresdefault.jpg' }
-                ];
-                this.bunnies = [
-                    { name: 'Fluffy', img: 'https://englishwoodlandstimber.files.wordpress.com/2012/05/fluffy-bunny-just-for-you.jpg' },
-                    { name: 'Red Eyes', img: 'https://40.media.tumblr.com/bd8c7ae7f4f60881c7780582afce097b/tumblr_n4w271cEsn1rfhxyho1_1280.jpg' }
+                this.movies = [
+                    { title: 'one', director: 'a' },
+                    { title: 'two', director: 'b' },
+                    { title: 'three', director: 'c' }
                 ];
             }
+            MovieController.prototype.pickColor = function (color) {
+                console.log(color);
+            };
             return MovieController;
         }());
         Controllers.MovieController = MovieController;
--- ngApp/controller.ts
+++ ngApp/controller.ts
@@ -1,20 +1,20 @@
+//controller
 namespace MovieApp.Controllers {
 
    export class MovieController{
-    public dogs;
-    public bunnies;
+    public movies;
 
-    constructor(){
-      this.dogs = [
-        {name: 'German Shepherd', img: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Buck_The_GSD.jpg'},
-        {name: 'Black Lab', img: 'https://i.ytimg.com/vi/GmXks--98Bc/maxresdefault.jpg'}
-      ]
-      this.bunnies = [
-        {name: 'Fluffy', img: 'https://englishwoodlandstimber.files.wordpress.com/2012/05/fluffy-bunny-just-for-you.jpg'},
-        {name: 'Red Eyes', img: 'https://40.media.tumblr.com/bd8c7ae7f4f60881c7780582afce097b/tumblr_n4w271cEsn1rfhxyho1_1280.jpg'}
-      ]
+    public pickColor(color:string){
+      console.log(color);
     }
 
+    constructor(){
+      this.movies = [
+        {title: 'one', director: 'a'},
+        {title: 'two', director: 'b'},
+        {title: 'three', director: 'c'}
+      ];
+    }
   }
   angular.module("MyApp").controller("MovieController", MovieController);
 }
